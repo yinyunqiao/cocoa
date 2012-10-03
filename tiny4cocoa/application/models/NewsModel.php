cd092fbbb6b684cbbe1ea1c4c3a85806d728a286
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2012-10-03 22:49:51 +0800

    初步实现显示首页和新闻具体页面

--- tiny4cocoa/application/models/NewsModel.php
+++ tiny4cocoa/application/models/NewsModel.php
@@ -5,41 +5,61 @@ class NewsModel extends baseDbModel {
     
     $start = ($page-1)*$pageSize;
     $sql = 
-		"SELECT * FROM `cocoacms_news`
-		ORDER BY `createdate` DESC 
-		limit $start,$pageSize;";
+    "SELECT * FROM `cocoacms_news`
+    ORDER BY `createdate` DESC 
+    limit $start,$pageSize;";
     $result =  $this->fetchArray($sql);
-	$ret = array();
-	if(count($result)==0)
-		return $ret;
-	foreach($result as $item) {
+    $ret = array();
+    if(count($result)==0)
+    return $ret;
+    foreach($result as $item) {
 		
-		$item["createtime"] = $this->countTime($item["createdate"]);
-		$ret[] = $item;
-	} 
-  	return $ret;
+      $item["createtime"] = $this->countTime($item["createdate"]);
+      $ret[] = $item;
+    } 
+    return $ret;
   }
-public static function countTime($time)
-{
-	$diff = time() - $time;
-	if($diff<0) {
-		$ret = "（时间不确）";
-	}else if($diff<60) {
-		$ret = $diff . "秒前";
-	} else if($diff<3600) {
-		$ret = floor($diff/60) . "分钟前";
-  	} else if($diff<3600*24) {
-  		$ret = floor($diff/3600) . "小时前";
-  	} else if($diff<3600*24*7) {
-  		$ret = floor($diff/3600/24) . "天前";
-  	} else if($diff<3600*24*30) {
-  		$ret = floor($diff/3600/24/7) . "周前";
-  	} else if($diff<3600*24*30*12) {
-  		$ret = floor($diff/3600/24/30) . "月前";
-  	} else {
-  		$ret = date("Y年m月d日",$time);
-  	}
-  	return $ret;
-}
+  
+  public function oneNews($index) {
+    
+    $sql =
+      "SELECT * FROM `cocoacms_news`
+      WHERE `id` = $index;";
+    $ret = $this->fetchArray($sql);
+    $news = $ret[0];
+    $news["createtime"] = $this->countTime($news["createdate"]);
+    $news["content"] = $this->toHtml($news["content"]);
+    return $news;
+  }
+  
+  public function toHtml($content) {
+
+    $content = str_replace("\r\n","<br/>",$content);
+    $content = str_replace("\n","<br/>",$content);
+    $content = str_replace("\r","<br/>",$content);
+    return $content;
+  }
+  public function countTime($time)
+  {
+    $diff = time() - $time;
+    if($diff<0) {
+      $ret = "（时间不确）";
+    }else if($diff<60) {
+      $ret = $diff . "秒前";
+    } else if($diff<3600) {
+      $ret = floor($diff/60) . "分钟前";
+    } else if($diff<3600*24) {
+      $ret = floor($diff/3600) . "小时前";
+    } else if($diff<3600*24*7) {
+      $ret = floor($diff/3600/24) . "天前";
+    } else if($diff<3600*24*30) {
+      $ret = floor($diff/3600/24/7) . "周前";
+    } else if($diff<3600*24*30*12) {
+      $ret = floor($diff/3600/24/30) . "月前";
+    } else {
+      $ret = date("Y年m月d日",$time);
+    }
+    return $ret;
+  }
     
 }
