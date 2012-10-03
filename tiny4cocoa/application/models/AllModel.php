c33581bf73fd0427c98a8e4dda3b020169b2537e
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2012-10-03 15:03:44 +0800

    初步完成新首页

--- tiny4cocoa/application/models/AllModel.php
+++ tiny4cocoa/application/models/AllModel.php
@@ -7,4 +7,12 @@ class AllModel extends baseDbModel {
     $sql = "SELECT * FROM `cocoabbs_threads` ORDER BY `lastpost` DESC limit $start,$pageSize;";
     return $this->fetchArray($sql);
   }
+  
+  public function newThreads($page,$pageSize) {
+    
+    $start = ($page-1)*$pageSize;
+    $sql = "SELECT * FROM `cocoabbs_threads` ORDER BY `dateline` DESC limit $start,$pageSize;";
+    return $this->fetchArray($sql);
+  }
+  
 }
--- tiny4wiki/index.php
+++ tiny4wiki/index.php
@@ -1,7 +1,6 @@
 <?php 
 define('IN_DISCUZ', TRUE);
 define('UC_API', TRUE);
-
 require_once '../config.inc.php';
 require_once '../uc_client/client.php';
 require_once 'app/index.php';
