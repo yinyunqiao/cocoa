02d36f88c545bfcfdb9a79d61463c2e2128931f5
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-01-04 14:55:09 +0800

    开始测试大头的分类系统

--- tiny4cocoa/application/controllers/HomeController.php
+++ tiny4cocoa/application/controllers/HomeController.php
@@ -243,6 +243,7 @@ class HomeController extends baseController
       // echo "$id = $ret<br/>";
       $url = "http://tiny4cocoa.com:9090/api/news/?id=$id";
       $ret = ToolModel::getUrl($url);
+      var_dump($ret);
       $newscenter->markApple($id,$ret);
     }
   }
