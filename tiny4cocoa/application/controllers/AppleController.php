61459e9b3f7ba3c349596f5d820b9d7fcbc55d0a
remy <lognorder@gmail.com>
2013-01-24 21:14:56 +0800

    finish rss

--- tiny4cocoa/application/controllers/AppleController.php
+++ tiny4cocoa/application/controllers/AppleController.php
@@ -160,9 +160,21 @@ class AppleController extends baseController
 
   public function rssAction() {
     
-    $newsModel = new NewsModel();
-    $news = $newsModel->news(1,30);
-    $this->_mainContent->assign("allnews",$news);
+    $newsModel = new NewscenterModel();
+    $news = $newsModel->news(1,30,"apple");
+    $napplenews = array();
+    if(count($news)>0) {
+      foreach($news as $item) {
+      
+        $item["time"] = ToolModel::countTime($item["pubdate"]);
+        $item["elink"] = urlencode($item["link"]);
+        $item["desc"] = ToolModel::summary($item["content"],300);
+        $napplenews[] = $item;
+      }
+      $news = $napplenews;
+    }
+    $this->_mainContent->assign("allnews",$news);   
+
     $this->_layout = "empty";
     $this->display();
   }
@@ -292,61 +304,6 @@ class AppleController extends baseController
     $this->_mainContent->assign("content",$news["content"]);
     $this->_layout = "flat";
     $this->display();
-  }
-
-  public function rssfeedAction(){
-    include ("FeedWriter/FeedTypes.php");
-    // $feedWriter = new FeedWriter(RSS2);
-    $rssFeeder = new RSS1FeedWriter();
-
-  //Setting the channel elements
-  //Use wrapper functions for common elements
-  //For other optional channel elements, use setChannelElement() function
-    $rssFeeder->setTitle('Tiny4Cocoa');
-    $rssFeeder->setLink('http://tiny4cocoa.com/home/');
-    $rssFeeder->setDescription('Tiny4Cocoa 最新苹果新闻');
-
-  //It's important for RSS 1.0 
-    $rssFeeder->setChannelAbout('http://tiny4cocoa.com/apple/rss');
-
-  //Adding a feed. Genarally this protion will be in a loop and add all feeds.
-
-  //Create an empty FeedItem
-  //   $newItem = $rssFeeder->createNewItem();
-
-  // //Add elements to the feed item
-  // //Use wrapper functions to add common feed elements
-  //   $newItem->setTitle('The first feed');
-  //   $newItem->setLink('http://www.yahoo.com');
-  // //The parameter is a timestamp for setDate() function
-  //   $newItem->setDate(time());
-  //   $newItem->setDescription('This is test of adding CDATA encoded description by the php <b>Universal Feed Writer</b> class');
-  // //Use core addElement() function for other supported optional elements
-  //   $newItem->addElement('dc:subject', 'Nothing but test');
-
-  // //Now add the feed item
-  //   $rssFeeder->addItem($newItem);
-
-  // //Adding multiple elements from array
-  // //Elements which have an attribute cannot be added by this way
-  //   $newItem = $rssFeeder->createNewItem();
-  //   $newItem->addElementArray(array('title'=>'The 2nd feed', 'link'=>'http://www.google.com', 'description'=>'This is a test of the FeedWriter class'));
-  //   $rssFeeder->addItem($newItem);
-    // $rssFeeder->generateFeed();
-    $newsModel = new NewsModel();
-    $news = $newsModel->news(1,30);
-    $news_root_url = "http://tiny4cocoa.com/home/s/";
-    foreach ($news as $a_news) {
-      $newItem = $rssFeeder->createNewItem();
-      $newItem->setTitle($a_news["title"]);
-      $a_news_id = $a_news["id"];
-      $a_news_link = $news_root_url.$a_news_id;
-      $newItem->setLink($a_news_link);
-      $newItem->setDate(time());
-      $newItem->setDescription($a_news["content"]);
-      $rssFeeder->addItem($newItem);
-    }
-    $rssFeeder->generateFeed();
   }
 }
 
