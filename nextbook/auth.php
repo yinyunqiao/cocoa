90f4831aeed6e068ea52a240950d170410e561c2
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-03-06 18:46:15 +0800

    nextbook api key初步验证上线

---
+++ nextbook/auth.php
@@ -0,0 +1,20 @@
+<?php
+$user["developerID"] = "chyhfj";
+$user["developerKey"] = "f2fe1124af14a9c8592a210eeb287b2a";
+$users[] = $user;
+$user["developerID"] = "yarshure";
+$user["developerKey"] = "07a0914041403c09bcef7b491a49c1f6";
+$users[] = $user;
+
+$data = file_get_contents('php://input');
+$array = json_decode($data,true);
+
+foreach($users as $u) {
+  
+  if($array["developerID"]==$u["developerID"]) {
+    
+    if($array["developerKey"]==$u["developerKey"])
+      die("ok");
+  }
+}
+die("Denied");
