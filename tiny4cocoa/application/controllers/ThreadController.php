2f09609ddf413ebfd40897ca2571591256dabafe
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-07-19 01:14:41 +0800

    #15 讨论区发帖

--- tiny4cocoa/application/controllers/ThreadController.php
+++ tiny4cocoa/application/controllers/ThreadController.php
@@ -1,4 +1,3 @@
-
 <?php
 class ThreadController extends baseController
 {
@@ -27,7 +26,35 @@ class ThreadController extends baseController
     $this->setTitle($thread["title"]);
     $this->display();
   }
-  
+ 
+  public function newAction() {
+    
+    $discuz = new DiscuzModel();
+    $userModel = new UserModel();
+    $userid = $discuz->checklogin();
+    $username = $userModel->username($userid);
+    if($userid==0) {
+      header("location: /logging.php?action=login");
+      die();
+    }
+    if($_POST){
+      
+      $data = array();
+      $data["title"] = $_POST["title"];
+      $data["content"] = $_POST["content"];
+      $data["createby"] = $username;
+      $data["createbyid"] = $userid;
+      $data["createdate"] = time();
+      if(strlen($_POST["title"])>0)
+        if(strlen($_POST["content"])>0) {
+          $threadModel = new ThreadModel();
+          $threadid = $threadModel->newThread($data);
+          header("location: /thread/show/$threadid/");
+          die();
+      }
+    }
+    $this->display();
+  }
 }
 
 
