ffa290fafeb36bbb1029ad2de57e9dbb694f8b39
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-07-20 12:10:59 +0800

    #37 发帖回帖加上wmd编辑器支持

--- tiny4cocoa/application/controllers/StaticController.php
+++ tiny4cocoa/application/controllers/StaticController.php
@@ -12,10 +12,13 @@ class StaticController extends tinyApp_Controller
     header ("content-type: application/x-javascript; charset: utf-8");
     header('Pragma: ');
     header ("cache-control: max-age=600");
+    ob_start();
     $offset = 60 * 60 * 24;
     $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
     header ($expire);
     $path=$this->_pathinfo['base'].'/public/js';
+    $base = $this->_pathinfo['base'];
+    $path = $base.'/public/js';
     $bootstrap = $this->_pathinfo['base']."/public/bootstrap/js/";
     
     include("$path/jquery/jquery.min.js");
@@ -23,6 +26,8 @@ class StaticController extends tinyApp_Controller
     include("$path/jquery/jquery.form.js");
     include("$path/upload/upload.js");
     include("$bootstrap/bootstrap.min.js");
+    readfile("$base/lib/wmd/wmd.js");
+    readfile("$base/lib/wmd/showdown.js");
   }
 	
   public function cssAction() {
@@ -33,12 +38,14 @@ class StaticController extends tinyApp_Controller
     $offset = 60 * 60 * 24;
     $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
     header ($expire);
-    $path=$this->_pathinfo['base'].'/public/css';
+    $base = $this->_pathinfo['base'];
+    $path = $base.'/public/css';
     $bootstrap = $this->_pathinfo['base']."/public/bootstrap/css";
     include("$path/base.css");
     include("$path/thread.css");
     include("$bootstrap/bootstrap.min.css");
     include("$bootstrap/bootstrap-responsive.min.css");
+    include("$base/lib/wmd/wmd.css");
   }
 }
 
