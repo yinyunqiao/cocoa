81a6e57fb96d32837d3f64a0786361bf68d70140
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-08-05 00:51:15 +0800

    #60 Find Users who status is normal

--- tiny4cocoa/application/models/UserModel.php
+++ tiny4cocoa/application/models/UserModel.php
@@ -8,4 +8,21 @@ class UserModel extends baseDbModel {
     return $result[0]["username"];
   }
   
+  public function normalUsers() {
+    
+    //groupid
+    //8:等待验证
+    //4:禁止发言
+    //5:禁止访问
+    //6:禁止IP
+    $sql="SELECT `username`,`email` 
+      FROM `cocoabbs_members`
+      WHERE `groupid` <> 4 
+	    AND `groupid` <> 5 
+	    AND `groupid` <> 6 
+	    AND `groupid` <> 8
+      AND `email` not in (SELECT `email` FROM `nonews`);";
+    $result = $this->fetchArray($sql);
+    return $result;
+  }
 }
