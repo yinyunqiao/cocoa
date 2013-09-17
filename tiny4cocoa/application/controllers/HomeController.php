9df5b4014f25bef22173f7630c5e03f0c44cb9c5
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-09-17 08:44:57 +0800

    hotfix

--- tiny4cocoa/application/controllers/HomeController.php
+++ tiny4cocoa/application/controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends baseController
     $count = $newscenter->count("apple");
     $newscount = $newscenter->count("unmarked");
     $spamcount = $newsModel->spamCount();
-    $applenews = $newscenter->news(1,23,"apple");
+    $applenews = $newscenter->news(1,22,"apple");
     $napplenews = array();
     foreach($applenews as $item) {
       
