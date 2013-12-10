73a79457f7635f86f3c23a29c5b4a3ed9bd2096c
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-12-10 16:14:25 +0800

    fixed #142

--- tiny4cocoa/application/models/UserModel.php
+++ tiny4cocoa/application/models/UserModel.php
@@ -51,15 +51,23 @@ class UserModel extends baseDbModel {
       ->fetchOne();
     if(!$user)
       return 0;
+    if($user["validated"]==1)
+      return 1;
     $v1 = md5($user["username"].$user["salt"].$user["email"]);
     if($v==$v) {
       
       $sql = "UPDATE `cocoabbs_uc_members` set `validated` = 1 WHERE `uid` = $userid;";
       $this->run($sql);
+      
+      $time = time();
+      $this->add_reputation($userid,10,"邮件验证成功",$time);
+      $this->add_money($userid,10,"邮件验证成功",$time);
+      $this->update_reputationAndMoney($userid);
       return 1;
     }
     return 0;
   }
+  
   public function sendValidateMail($userid) {
     
     $user = $this->select("cocoabbs_uc_members")
@@ -383,6 +391,34 @@ class UserModel extends baseDbModel {
               (SELECT count(*)   `c`,`userid` FROM `thread_replys` GROUP BY `userid`) `tempt`
             ON `tempt`.`userid` = `cocoabbs_uc_members`.`uid` 
             SET `replys`=`c`;";
+    $this->run($sql);
+  }
+  
+  public function add_reputation($userid,$amount,$message,$time) {
+    
+    $data["userid"] = $userid;
+    $data["amount"] = $amount;
+    $data["message"] = $message;
+    $data["updatetime"] = $time;
+    $this->select("reputation")->insert($data);
+  }
+  
+  public function add_money($userid,$amount,$message,$time) {
+    
+    $data["userid"] = $userid;
+    $data["amount"] = $amount;
+    $data["message"] = $message;
+    $data["updatetime"] = $time;
+    $this->select("money")->insert($data);
+  }
+  
+  public function update_reputationAndMoney($userid) {
+    
+    $sql = "UPDATE `cocoabbs_uc_members` 
+            SET 
+              `reputation` = (SELECT SUM(`amount`) FROM `reputation` WHERE `userid` = $userid),
+              `money` = (SELECT SUM(`amount`) FROM `money` WHERE `userid` = $userid)
+            WHERE `uid` = $userid;";
     $this->run($sql);
   }
 }
